0222356432b03497f9c6e41736cf4cfae5944d01
Heart Wirunrom <51272607+heart569522@users.noreply.github.com>
2021-02-04 13:09:27 +0700

    Update view.fetchAll.inc.php

--- app/view/view.fetchAll.inc.php
+++ app/view/view.fetchAll.inc.php
@@ -3,14 +3,14 @@
     <a class="btn btn-primary" href="?add_data=allow">Add Common Stock</a>
     <table class="table table-striped table-hover">
         <thead class="table-dark">
-            <th scope="col">ID</th>
-            <th scope="col">Symbol</th>
-            <th scope="col">Price</th>
-            <th scope="col">Common</th>
+            <th width="10%" scope="col">ID</th>
+            <th width="30%" scope="col">Symbol</th>
+            <th width="30%" scope="col">Price</th>
+            <th width="30%" scope="col">Common</th>
         </thead>
         <tbody>
             <?php
-            $data->fetchAll();
+                $data->fetchAll();
             ?>
         </tbody>
     </table>
