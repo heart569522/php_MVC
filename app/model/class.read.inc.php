29174ac67b1283d94df1a3add8d58b6776cafe89
Heart Wirunrom <51272607+heart569522@users.noreply.github.com>
2021-02-11 10:32:09 +0700

    Update class.read.inc.php

--- app/model/class.read.inc.php
+++ app/model/class.read.inc.php
@@ -4,14 +4,14 @@
             $this->statement = $this->connect->prepare("SELECT * FROM common_stock");
             $this->statement->execute();
             $data = $this->statement->get_result()->fetch_all(MYSQLI_ASSOC);
-            foreach ($data as $row){
+            foreach ($data as $datas){
                 echo "<tr>
-                        <th class='text-center' scope='row'>".$row['id']."</th>
-                        <td> <a href='?id={$row['id']}'>".$row['quote']."</a> </td>
-                        <td>".$row['price']."</td>
+                        <th class='text-center' scope='row'>".$datas['id']."</th>
+                        <td> <a href='?id={$datas['id']}'>".$datas['quote']."</a> </td>
+                        <td>".$datas['price']."</td>
                         <td class='text-center'>
-                            <a class='btn btn-danger' href='?idd={$row['id']}&delete=allow'>Delete</a>
-                            <a class='btn btn-warning' href='?idu={$row['id']}&edit=allow&update=disallow'>Edit</a>
+                            <a class='btn btn-danger' href='?idd={$datas['id']}&delete=allow'>Delete</a>
+                            <a class='btn btn-warning' href='?idu={$datas['id']}&edit=allow&update=disallow'>Edit</a>
                         </td>
                       </tr>";
                   
