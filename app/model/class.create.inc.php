e26d946a8c4015665a660d7273801673de5bd7a4
Heart Wirunrom <51272607+heart569522@users.noreply.github.com>
2021-02-18 10:42:32 +0700

    Update class.create.inc.php

--- app/model/class.create.inc.php
+++ app/model/class.create.inc.php
@@ -3,7 +3,16 @@
         public function createData($quote, $price) {
             $this->statement = $this->connect->prepare("INSERT INTO common_stock (quote, price) VALUES (?, ?)");
             $this->statement->bind_param("sd", $quote, $price);
+            if($this->statement->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
 
+        public function createPicture($idcom, $name, $path) {
+            $this->statement = $this->connect->prepare("INSERT INTO picture (idcom, name, path) VALUES (?, ?, ?)");
+            $this->statement->bind_param("iss", $idcom, $name, $path);
             if($this->statement->execute()) {
                 return true;
             } else {
