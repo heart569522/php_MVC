4d5b73685e13773516a05f4b9b4ac6df7f548889
Heart Wirunrom <51272607+heart569522@users.noreply.github.com>
2021-02-18 11:04:54 +0700

    Update view.importPicture.inc.php

--- app/view/view.importPicture.inc.php
+++ app/view/view.importPicture.inc.php
@@ -1,5 +1,15 @@
 <div class="container">
     <h1>Import Files</h1>
     <hr>
-    
+    <form action="?upload=allow" method="post" enctype="multipart/form-data">
+        <input type="file" name="fileUpload[]" id="fileUpload" multiple="mutiple">
+        <button class="btn btn-success" type="submit">Upload</button>
+    </form>
+    <hr>
+    <button onclick="back()">Back</button>
+    <script>
+        function back(){
+            window.history.back();
+        }
+    </script>
 </div>
